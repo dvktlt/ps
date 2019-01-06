918acc7fe1be7ffabb720e7fe1cdce312279e3aa
dvktlt <36969054+dvktlt@users.noreply.github.com>
2019-01-06 19:45:10 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,61 @@
+var jsonData = [
+    {
+        "q" : "1. What's the capital of the Czech Republic?",
+        "opt1" : "Brno",
+        "opt2" : "Olomouc",
+        "opt3" : "Praha",
+        "answer" : "Praha"
+    },
+    {
+        "q" : "2.What are the neighbouring countries in the CZ?",
+        "opt1" : "Russia, Germany, Poland, Spain",
+        "opt2" : "Germany, Slovakia, Poland, Ukraine",
+        "opt3" : "Poland, Slovakia, Germany, Austria",
+        "answer" : "Poland, Slovakia, Germany, Austria"
+    },
+    {
+        "q" : "3. What is the currency in the Czech Republic?",
+        "opt1" : "Czech koruna",
+        "opt2" : "Czech dollar",
+        "opt3" : "Czech pound",
+        "answer" : "Czech koruna"
+    },
+    {
+        "q" : "4. What was the name of the 1st president of the Czech Republic?",
+        "opt1" : "J. A. Komenský",
+        "opt2" : "Václav Havel",
+        "opt3" : "Václav Klaus",
+        "answer" : "Václav Havel"
+    },
+    {
+        "q" : "5. What's the name of the three parts of the CZ?",
+        "opt1" : "Bavaria, Moravia, Silesia",
+        "opt2" : "Bohemia, Moravia, Slovenia",
+        "opt3" : "Bohemia, Moravia, Silesia",
+        "answer" : "Bohemia, Moravia, Slovenia"
+    },
+{
+        "q" : "6. In what year was the Czechoslovakia divided into Slovakia and the Czech Republic?",
+        "opt1" : "1993",
+        "opt2" : "1918",
+        "opt3" : "1989",
+        "answer" : "1993"
+    },
+{
+        "q" : "7. How many inhabitants live in the CZ? ",
+        "opt1" : "7 600 000",
+        "opt2" : "24 000 000",
+        "opt3" : "10 500 000",
+        "answer" : "10 500 000"
+    },
+{
+        "q" : "8. What is the highest point of the Czech Republic?",
+        "opt1" : "Praded",
+        "opt2" : "Snežka",
+        "opt3" : "Lysá hora ",
+        "answer" : "Snežka"
+    },
+
+
+
+];
---
+++ js.js
@@ -0,0 +1,144 @@
+const formular = document.getElementById("formular");
+
+    /*	$(document).ready(function(){
+				
+		if (localStorage.saveIt) {
+			
+			refreshNames(); 
+      refreshTable();
+      
+			
+		} else { 
+			$("#list").text("no bands yet");
+			
+		}
+		
+	});
+    */
+    var nxtBtn = document.getElementById("nxtBtn");
+
+    nxtBtn.addEventListener("click", checkAnswer);
+	
+	
+	
+	//aktualizuj seznam jmen
+	function refreshNames(){
+		var namesAsText = ""
+		
+		$.each(JSON.parse(localStorage.getItem('saveIt')), function(m, val) {
+		  namesAsText = namesAsText + "name: "+val.yourName+", result: "+val.result+"<br>";
+		});
+		
+		//aktualizujeme seznam jmen
+		$("#list").html(namesAsText);		
+	}
+
+
+
+reset.onclick = function(){
+location.reload();
+     access.disabled = true;
+      calc.disabled = false;
+      yourName.disabled = false;
+     
+     scroll(300,300);
+     document.getElementById('displayTable').style.display = 'none';
+ 
+}
+// json array movement variable
+    var i = 0;
+var correctCount = 0 ;
+
+//initialize the first question
+generate(i);
+// generate from json array data with index
+function generate(index) {
+    document.getElementById("question").innerHTML = jsonData[index].q;
+    document.getElementById("optt1").innerHTML = jsonData[index].opt1;
+    document.getElementById("optt2").innerHTML = jsonData[index].opt2;
+    document.getElementById("optt3").innerHTML = jsonData[index].opt3;
+}
+
+function checkAnswer(e) {
+    e.preventDefault();
+    console.log("test");
+
+    if (document.getElementById("opt1").checked && jsonData[i].opt1 == jsonData[i].answer) {
+       correctCount++;
+    }
+    if (document.getElementById("opt2").checked && jsonData[i].opt2 == jsonData[i].answer) {
+        correctCount++;
+    }
+    if (document.getElementById("opt3").checked && jsonData[i].opt3 == jsonData[i].answer) {
+        correctCount++;
+    }
+    // increment i for next question
+    i++;
+    if(jsonData.length-1 < i){
+        
+  		const scoreResult = document.getElementById("scoreResult");
+      scoreResult.innerHTML = `****Your score is: ${correctCount}*****`;
+      scoreResult.classList.toggle("scoreResultShow");
+        
+    }
+    // callback to generate
+    console.log(i);
+    generate(i);
+}
+function saveName(){
+	
+		
+		var yourName = $("#yourName").val();
+		var result = $("#result").val();
+		
+		saveIt = JSON.parse(localStorage.getItem('saveIt'));
+		    access.disabled = true;
+		if (!saveIt){ 
+			saveIt = JSON.parse('[]');
+		}
+		      
+    
+		
+		newName = {yourName: yourName, result: result}
+		
+		
+		saveIt.push(newName);
+		
+		localStorage.setItem("saveIt", JSON.stringify(saveIt));
+		
+    
+	   document.getElementById('displayTable').style.display = 'table';
+		
+		refreshNames();
+    refreshTable();
+	}
+
+calc.onclick =function(){
+scoreResult.innerHTML = `Your score is: ${correctCount}`;
+      scoreResult.classList.toggle("scoreResultShow");
+access.disabled = false;
+     calc.disabled = true;
+      yourName.disabled = true;
+
+}
+
+function refreshTable() {
+    let table = document.getElementById("list");
+    table.createTHead();
+    table.innerHTML = "<th>Name</th><th>Score</th>";
+    let scores = JSON.parse(localStorage.saveIt);
+    let rowNumber = 1;
+    for (let pomoc in scores) {
+        let row = table.insertRow(rowNumber);
+        let cell2 = row.insertCell(0);
+        let cell3 = row.insertCell(1);
+
+        cell2.innerText = scores[pomoc].yourName;
+        cell3.innerHTML = scores[pomoc].result;
+
+        rowNumber++;
+    }
+    
+}
+
+document.getElementById('displayTable').style.display = 'none';
